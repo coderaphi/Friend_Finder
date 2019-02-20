ba259695ba8835ee94a7403fb5960de5dc735b7c
Rasan <rashmedo@gmail.com>
2019-02-20 00:28:19 -0500

    final css cleanups

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,5 +1,6 @@
 // dependencies 
 var path = require("path");
+var express = require("express");
 
 
 // HTML Routes
@@ -7,12 +8,6 @@ var path = require("path");
 
 
 module.exports = function (app) {
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"))
-    });
-
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-
+    app.use(express.static(path.join(__dirname, '../public')));
+   
 };
